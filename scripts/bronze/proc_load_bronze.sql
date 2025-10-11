4e44b6e9e0e246e05e7add1a99a795bc224f77b7
Mahevish Fatima <mahevishf786@gmail.com>
2025-10-11 17:44:02 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -18,9 +18,24 @@
 
 -- This tells you which user needs permission to that folder.
 
--- ================================================================
--- CRM: Customer Info
--- ================================================================
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 
 BEGIN
@@ -197,3 +212,4 @@ END
 -- ================================================================
 
 -- EXEC bronze.load_bronze
+
